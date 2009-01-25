93021a75da3aeeb4392ef521858fa1b7f999b493
Suzuki Masahiro <cielacanth@luaforge.net>
2009-01-25 22:16:05 +0000

    Compiled on coLinux.

--- lldebug/src/precomp.h
+++ lldebug/src/precomp.h
@@ -61,9 +61,9 @@
 
 #if defined(LLDEBUG_CONTEXT)
 	extern "C" {
-	#include "lua/lua.h"
-	#include "lua/lualib.h"
-	#include "lua/lauxlib.h"
+	#include "lua.h"
+	#include "lualib.h"
+	#include "lauxlib.h"
 	}
 #endif
 #if defined(LLDEBUG_VISUAL)
